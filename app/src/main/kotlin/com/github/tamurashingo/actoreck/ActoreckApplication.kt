89c962d3ca8780ff2304daca0897ba05ca800998
tamura shingo <tamura.shingo@gmail.com>
2021-08-21 06:09:04 +0900

    create project

---
+++ app/src/main/kotlin/com/github/tamurashingo/actoreck/ActoreckApplication.kt
@@ -0,0 +1,10 @@
+package com.github.tamurashingo.actoreck
+
+import android.app.Application
+
+class ActoreckApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
